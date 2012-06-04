75a2f8733b53a4aeed942e7be53db9d90b64a414
Dave Levine <dmlevi@gmail.com>
2012-06-04 13:17:25 -0500

    setup controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,39 @@
 
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to @post, notice: "Post created"
+    else render :new
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attribues(params[:post])
+      redirect_to @post, notice: "Post updated"
+    else render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
   end
 end
