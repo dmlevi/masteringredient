1e0f3dace8ccb766816e83adeb3825f01e9c07f2
Dave Levine <dmlevi@gmail.com>
2012-06-04 17:20:02 -0500

    fixed database schema

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :description, :user_id, :avatar
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   belongs_to :user
+  
 end
