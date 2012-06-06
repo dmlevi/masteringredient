7e616669142504036f6d6aeef0f0646a8513ce4f
Dave Levine <dmlevi@gmail.com>
2012-06-05 19:34:00 -0500

    added title to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120604212638) do
+ActiveRecord::Schema.define(:version => 20120606000749) do
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20120604212638) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "title"
   end
 
   create_table "users", :force => true do |t|
