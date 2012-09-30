830fed09a95bbcad37ff070b1bf8167685c2e3fa
Dave Levine <dmlevi@gmail.com>
2012-09-30 18:17:35 -0400

    style changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessible :description, :user_id, :avatar
+  attr_accessible :description, :avatar, :title, :avatar
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+ 
   belongs_to :user
   
   validates_presence_of :description
